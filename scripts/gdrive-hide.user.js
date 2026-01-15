643b46762b512a4c1af13255a1867fb49f563152
yant0 <80298071+yant0@users.noreply.github.com.>
2026-01-15 08:44:31 +0700

    2026-01-15 : Make script work lol

--- scripts/gdrive-hide.user.js
+++ scripts/gdrive-hide.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Drive Hide Left Pane
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-11
+// @version      2026-01-15
 // @description  Hides left pane in Google Drive using the small "Hide side panel" on bottom right.
 // @author       Yant0
 // @match        https://drive.google.com/*
@@ -10,14 +10,20 @@
 // ==/UserScript==
 
 (function () {
-    hidden = false
-    left = document.querySelectorAll("div#drive-main-page > div > div:nth-child(n+4):nth-child(-n+5)")
-    btn = document.querySelector("div[aria-label='Hide side panel']")
-    btn.onclick = () => {
-        hidden = !hidden
-        if (hidden)
-            left.forEach(e => { e.style.display = 'none' })
-        else
-            left.forEach(e => { e.style.display = 'block' })
-    }
+    let hidden = false
+    let left, btn
+    const o = new MutationObserver(() => {
+        left = document.querySelectorAll("div#drive-main-page > div > div:nth-child(n+4):nth-child(-n+5)")
+        btn = document.querySelector("div[aria-label$='side panel']")
+        if (left && btn) {
+            btn.addEventListener("click", () => {
+                hidden = !hidden
+                if (hidden) left.forEach(e => { e.style.display = 'none' })
+                else left.forEach(e => { e.style.display = 'block' })
+            })
+            o.disconnect()
+        }
+    })
+
+    o.observe(document.body, { childList: true, subtree: true })
 })()
