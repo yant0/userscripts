4151a0c9dbacb245f0df769fa84c8a42500a6099
yant0 <80298071+yant0@users.noreply.github.com>
2026-03-01 06:36:53 +0700

    2026-03-01  : fix

--- scripts/ytm-lyrics.user.js
+++ scripts/ytm-lyrics.user.js
@@ -14,20 +14,18 @@
 (function () {
     'use strict';
 
-    // [ ] better lyrictab query
     // [ ] strip "official video/audio" and artist name
     // [ ] deobfuscate regex lmao
-    // [ ] style properly
     // [ ] highlight current line
-    // [ ] refacotr
 
-    let media, player_tab, lyric_tab, lyric_container, song, song_title, song_artist, song_album, song_length, last_song_fetched
+    let media, player_tab, lyric_tab, lyric_container, song, song_title, song_artist, song_album, song_length, last_song_fetched, custom_lyric
     let sync = []
     const s = document.createElement("style")
     s.textContent = `
         lyric {
             &, * {
-                padding-block: 1.5rem;
+                padding-block: 1.2rem;
+                margin-block: 1.2rem;
             }
             .synced {
                 cursor: pointer;
@@ -50,24 +48,20 @@
 
     function init() {
         enableTab(lyric_tab)
-        const i = setInterval(() => {
-            lyric_container = document.querySelector("ytmusic-tab-renderer > ytmusic-message-renderer")
-            if (lyric_container !== null) {
-                lyric_tab.onclick = (function () {
-                    if (lyric_container.lastChild.nodeName !== "LYRIC") { fetch_lyric() }
-                    else if (lyric_container.lastChild.getAttribute("song") !== song_title) { fetch_lyric() }
-                })()
-                clearInterval(i)
-            }
-        }, 500);
+        custom_lyric = document.querySelector("lyric")
+        lyric_tab.onclick = function () {
+            if (!custom_lyric) { fetch_lyric() }
+            else if (custom_lyric.getAttribute("song") !== song_title) { fetch_lyric() }
+        }
         const tab_observer = new MutationObserver(() => { enableTab(lyric_tab) })
         tab_observer.observe(lyric_tab, { attributes: true })
 
         const song_observer = new MutationObserver(() => {
-            if (lyric_container.lastChild.nodeName === "LYRIC") {
-                lyric_container.lastElementChild.remove()
+            custom_lyric = document.querySelector("lyric")
+            if (custom_lyric) {
+                custom_lyric.remove()
             }
-            if (lyric_tab.getAttribute("aria-selected")) fetch_lyric();
+            if (lyric_tab.getAttribute("aria-selected") === "true") { fetch_lyric() }
         })
         song_observer.observe(player_tab, { childList: true, subtree: true })
     }
@@ -82,11 +76,17 @@
             + `${song_album !== null ? `&album_name=${song_album}` : ""}`
             + `&track_name=${song_title}`
             + `&duration=${song_length}`)
-            .then(r => r.json()
-                .then(lyrics => {
-                    console.log(lyrics);
-                    setLyrics(lyrics)
-                }))
+            .then(response => {
+                if (response.ok) { return response.json() }
+                throw new Error("Lyric not found");
+            })
+            .then(lrclib => {
+                console.log(lrclib);
+                setLyrics(lrclib)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     function toSeconds(song_length) {
@@ -109,10 +109,17 @@
         tab.removeAttribute("disabled")
     }
 
-    function setLyrics(lyrics) {
-        [...lyric_container.children].forEach(e => { e.style.display = "none" });
-        if (lyrics.syncedLyrics) { lyric_container.appendChild(formatLyrics(true)) }
-        else if (lyrics.plainLyrics) { lyric_container.appendChild(formatLyrics(false)) }
+    function setLyrics(lrclib) {
+        const i = setInterval(() => { // this looks fucked
+            console.log(lyric_container);
+            setLyricContainer()
+            if (lyric_container !== null && lyric_container !== undefined) {
+                [...lyric_container.children].forEach(e => { e.style.display = "none" });
+                if (lrclib.syncedLyrics) { lyric_container.insertAdjacentElement("afterbegin", formatLyrics(true)) }
+                else if (lrclib.plainLyrics) { lyric_container.insertAdjacentElement("afterbegin", formatLyrics(false)) }
+                clearInterval(i)
+            }
+        }, 1000);
 
         function formatLyrics(synced) {
             media = document.querySelector("video")
@@ -120,7 +127,7 @@
             if (synced) {
                 // i need help fixing this regex, the fourth capture should return how many newlines. "/n/n"
                 // or this shit could be parsed line by line
-                const lyric_meta = [...lyrics.syncedLyrics.matchAll(/\[0?([\d:.]+).+?((?=\s).+?(?=\[)| )/gs)]
+                const lyric_meta = [...lrclib.syncedLyrics.matchAll(/\[0?([\d:.]+).+?((?=\s).+?(?=\[)| )/gs)]
                 for (const [, time, line] of lyric_meta) {
 
                     const lyric_element = document.createElement("div")
@@ -139,7 +146,7 @@
                     }
                 }
             } else {
-                lyric.innerHTML = lyrics.plainLyrics.split("\n").join("<br>")
+                lyric.innerHTML = lrclib.plainLyrics.split("\n").join("<br>")
             }
             lyric.setAttribute("song", song_title)
             lyric.style.fontSize = "2rem"
@@ -169,6 +176,15 @@
             }
         })
         o.observe(time_query, { characterData: true })
+    }
+
+    function setLyricContainer() {
+        let og_lyrics = document.querySelector("ytmusic-section-list-renderer[page-type$=TRACK_LYRICS]")
+        if (og_lyrics && og_lyrics.style.display !== "none") {
+            lyric_container = og_lyrics
+        } else {
+            lyric_container = document.querySelector("ytmusic-tab-renderer > ytmusic-message-renderer")
+        }
     }
 
 })();
